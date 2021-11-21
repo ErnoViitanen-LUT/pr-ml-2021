b064f8021e9fe30045fbeefff0d3bb4fa1930fb6
Erno Viitanen <erno.viitanen@student.lut.fi>
2021-11-21 13:03:25 +0200

    option to plot individual digit

--- main.m
+++ main.m
@@ -1,11 +1,13 @@
 loadstrokes;
 
+DEBUG = true;
 if ~exist('DEBUG', 'var')
         DEBUG = false;
 end
 
 startIndex = 80;
+plotDigit = 1;
 data=datanormalization(data);
 if(DEBUG==true)
-    plotstrokes(data,class,startIndex);
+    plotstrokes(data,class,startIndex,plotDigit);
 end

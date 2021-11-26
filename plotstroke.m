cb4bd9b059b2661fbf2932172dbfebc404c221d8
Erno Viitanen <erno.viitanen@student.lut.fi>
2021-11-27 00:44:09 +0200

    interactive digit input

--- plotstroke.m
+++ plotstroke.m
@@ -1,4 +1,4 @@
-function plotstrokes(data,class,sampleIndex,plotDigit)
+function plotstrokes(data)
     % plot strokes starting from sampleIndex
     % plotDigit [optional]: plot alternatively one digit multiple times
     
@@ -6,82 +6,21 @@ function plotstrokes(data,class,sampleIndex,plotDigit)
         sampleIndex = 1;
     end
    
-    hFig = figure;
-    hFig.Position = [100 100 1400 800]; % set figure position and size
+    hFig = figure;   
+    sample = data;
+    plot(sample(:,1), sample(:,2));
+    hold on;
+    [pos,intersection,segment,interpos] = plotrects(sample);    
+    title("2D","start-end: " + pos(1) +":"+ pos(2) + ", intersections: " + mat2str(interpos));
 
-    % Create UI controls and callbacks for changing stroke set
-    ButtonPrev = uicontrol('Parent',hFig,'Style','pushbutton','String','Prev','Units','points','Position',[420 20 40 20],'Visible','on');
-    TextIndex=uicontrol('Parent',hFig,'Style','text','String',sampleIndex,'Units','points','Position',[460 20 50 20],'Visible','on');
-    ButtonNext=uicontrol('Parent',hFig,'Style','pushbutton','String','Next','Units','points','Position',[500 20 50 20],'Visible','on');
-    
-    if exist('plotDigit', 'var')
-        ButtonNext.Callback = {@btn_click,TextIndex,data,1,plotDigit};
-        ButtonPrev.Callback = {@btn_click,TextIndex,data,-1,plotDigit};
-        plot_digit(data,sampleIndex,plotDigit); % plot individual digit
-    else
-        ButtonNext.Callback = {@btn_click,TextIndex,data,1};
-        ButtonPrev.Callback = {@btn_click,TextIndex,data,-1};
-        draw_plots(data,sampleIndex); % plot different digits
-    end
-end
-function draw_plots(data,sampleIndex)
-    % plot different digits
-    samplesFrom = sampleIndex;
-    samplesTo = sampleIndex;
-    
-
-    cla; % clear subplots
-    for j=samplesFrom:samplesTo
-        for i=0:9 % plot 10 digits in 3D
-            sample = cell2mat(data(i*100 + j));
-            subplot(4,5,i+1);            
-            plot3(sample(:,1), sample(:,2), sample(:,3), 'k-');
-            view(360,90); % rotate plot
-            hold on;
-
-            [pos,intersection,segment,interpos] = plotrects(sample);
-            title("3D " + (i*100 + j),"start-end: " + pos(1) +":"+ pos(2) + ", intersections: " + mat2str(interpos));
-            hold off;
-        end
-        for i=0:9 % plot 10 digits in 2D
-            sample = cell2mat(data(i*100 + j));
-            subplot(4,5,i+10+1);
-            plot(sample(:,1), sample(:,2));
-            hold on;
-            [pos,intersection,segment,interpos] = plotrects(sample);
-            
-            title("2D " + (i*100 + j),"start-end: " + pos(1) +":"+ pos(2) + ", intersections: " + mat2str(interpos));
-            hold off;
-        end  
-    end
-    drawnow;  
-end
-
-function plot_digit(data,sampleIndex,plotDigit)
-    % plot individual digit
-    
-    cla; % clear subplots
-    for j=1:20 % plot 20 digits in 2D
-        sample = cell2mat(data(plotDigit*100 + j + sampleIndex));
-        subplot(4,5,j);
-        plot(sample(:,1), sample(:,2));
-        hold on;
-        [pos,intersection,segment,interpos] = plotrects(sample);
-
-        
-        title("2D " + (plotDigit*100 + j + sampleIndex),"start-end: " + pos(1) +":"+ pos(2) + ", intersections: " + mat2str(interpos));
-
-        hold off;
-    end
-
-    drawnow;    
+    hold off;
 end
 
 function [pos,intersection,segment,interpos] = plotrects(sample)
     % plot rectangles for start and end samples 
     intersection = nan;
     interpos = 0;
-    numPointsInSample = 3;
+    numPointsInSample = 2;
     sampleStart=sample(1:numPointsInSample,:);
     sampleSize = size(sample,1);
     sampleEnd = sample(sampleSize - numPointsInSample:sampleSize,:);
@@ -129,9 +68,7 @@ function [pos,intersection,segment,interpos] = plotrects(sample)
         if pos(2) == 0
             pos(2) = plotOneRect(sampleEnd(1,:),k,"blue");
         end
-    end
-    pos
-    
+    end    
 end
 
 function foundInPos = plotOneRect(sample,pos,color)

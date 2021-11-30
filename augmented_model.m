48a66f7f78f9a069486309f5fda4b93b1cdc3132
WojciechMojsiejuk <wojciechmojsiejuk@gmail.com>
2021-11-30 22:39:07 +0200

    added testing the training acc (no normalization yet)

--- augmented_model.m
+++ augmented_model.m
@@ -1,4 +1,4 @@
-function [model, C, y_test, acc] = augmented_model(data,class,train_size,dim, seed)
+function [model, C, y_test, acc, acc_total] = augmented_model(data,class,train_size,dim, seed)
 % AUGMENTEDSTATISTICALMODEL(data, class, train_size, dim, seed)
     DEBUG=true;
     if ~exist('DEBUG', 'var') %checks if DEBUG variable exists, if not we assume DEBUG = false
@@ -30,15 +30,15 @@ function [model, C, y_test, acc] = augmented_model(data,class,train_size,dim, se
         error("Train size must be a value from (0,1)");
     end
     
-    if(DEBUG==true)
-%         close all; % if debug is true, close all figures generated in the previous run
-    end
+%     if(DEBUG==true)
+% %         close all; % if debug is true, close all figures generated in the previous run
+%     end
     
     
     %calculating the sizes of training and testing data
     train_data_size = round(train_size*data_size);
     train_data = zeros(dim,train_data_size);
-    %if train size is different from 1 it means we want to also test the model
+    train_class = zeros(1,train_data_size);
     test_data = zeros(dim,data_size-train_data_size);
     test_class = zeros(1,data_size - train_data_size);
     [~,test_data_size] = size(test_data);
@@ -54,6 +54,7 @@ function [model, C, y_test, acc] = augmented_model(data,class,train_size,dim, se
 %             figure
         end
         for i = 1:N_train
+            train_class(:,digit*N_train+i) = digit; 
             x = cell2mat(x_train(i)); %we convert our data from cell to matrix
             if(isempty(x))
                 train_data(:,digit*N_train+i)=NaN(1,dim);
@@ -118,6 +119,28 @@ function [model, C, y_test, acc] = augmented_model(data,class,train_size,dim, se
     %Computing bayesian classifiers models%
     model = bayesian_classifiers();
     
+    %Checkin accuracy of training model
+    C = zeros(10,train_data_size);
+    for j=1:train_data_size
+        for k=0:9
+            s = strcat("digit_",num2str(k));
+            try
+%                 C(k+1,j) = discriminant(test_data(:,j), model.(s).mu,model.(s).sigma);
+                C(k+1,j) =  mvnpdf(train_data(:,j),model.(s).mu,model.(s).sigma); %we calculate the density function for test data with each model
+            catch ME
+                C(k+1,j) = 0;
+            end
+        end
+    end
+    [~,y_train] = max(C,[],1); %we choose the class which has the biggest value
+    y_train = y_train - 1; %because argmax would be from 1 to 10 we subtract 1 to go back to 0 to 9
+    acc_train = zeros(1,10);
+    for i = 1:10 
+        err = sum(y_train(test_class==i-1) ~= train_class(test_class==i-1)); % we compare our results with the expected values
+        acc_train(i) = (train_data_size/10-err)/(train_data_size/10); % we compute the accuracy
+    end
+    err = sum(y_train ~= train_class); % we compare our results with the expected values
+    acc_training_total = (train_data_size-err)/(train_data_size) % we compute the accuracy
     
     %Checking test data 
 
@@ -135,12 +158,18 @@ function [model, C, y_test, acc] = augmented_model(data,class,train_size,dim, se
     end
     [~,y_test] = max(C,[],1); %we choose the class which has the biggest value
     y_test = y_test - 1; %because argmax would be from 1 to 10 we subtract 1 to go back to 0 to 9
-    error = sum(y_test ~= test_class); % we compare our results with the expected values
-    acc = (test_data_size-error)/(test_data_size) % we compute the accuracy
+    acc = zeros(1,10);
+    for i = 1:10 
+        err = sum(y_test(test_class==i-1) ~= test_class(test_class==i-1)); % we compare our results with the expected values
+        acc(i) = (test_data_size/10-err)/(test_data_size/10); % we compute the accuracy
+    end
+    err = sum(y_test ~= test_class); % we compare our results with the expected values
+    acc_total = (test_data_size-err)/(test_data_size) % we compute the accuracy
     if(DEBUG==true)
         figure
         confusion = confusionmat(test_class,y_test); %we compute the confusion matrix
         confusionchart(confusion)
+        title('Augmented model')
     end  
     
     function model = bayesian_classifiers
--- majority_voting_ensemble.m
+++ majority_voting_ensemble.m
@@ -1,4 +1,4 @@
-function majority_voting_ensemble(C1,y_test1,acc1,C2,y_test2,acc2)
+function majority_voting_ensemble(C1,y_test1,acc1,acc1_total,C2,y_test2,acc2,acc2_total)
 %Let's assume model accuracy and how sure it is be defined by the variable x
 % be presented as [digit,acc,sureness] = model
 %then we need to find such a function that will boost high accuracy and
@@ -7,11 +7,11 @@ function majority_voting_ensemble(C1,y_test1,acc1,C2,y_test2,acc2)
 %is the function y. We choose the digit where y is the highest
 test_class = zeros(1,length(y_test1));
 test_data_size = length(test_class);
-C2 = reshape(C2,10,500);
+C2 = reshape(C2,10,test_data_size);
 DEBUG=true;
 % y1 = exp(1./(1-acc1));
 % y2 = exp(1./(1-acc2));
-C = C1*acc1 + C2*acc2;
+C = C1.*(acc1'.*acc1_total) + C2.*(acc2'.*acc2_total);
 [~,y_test] = max(C,[],1); %we choose the class which has the biggest value
 y_test = y_test - 1; 
 
@@ -25,6 +25,7 @@ if(DEBUG == true)
     figure
     confusion = confusionmat(test_class,y_test); %we compute the confusion matrix
     confusionchart(confusion)
+    title('Voting')
 end
 
 end
